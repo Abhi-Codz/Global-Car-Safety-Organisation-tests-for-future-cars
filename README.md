3a42faed6630ec9d878a68e46cf9d6d033e49100
Abhinavprogrammer <71148737+Abhinavprogrammer@users.noreply.github.com>
2020-10-31 12:51:34 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Global-Car-Safety-Organisation-tests-for-future-cars
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,57 @@
+//Naming variables for main sprites
+var car, wall;
+var speed, weight;
+
+function setup() 
+{
+  createCanvas(1600, 400);
+
+  //Adding random values to these actions
+  speed = random(55, 90);
+  weight = random(400, 1500);
+
+  //Creating sprite for car
+  car = createSprite(50, 200, 50, 50);
+  car.velocityX = speed;
+  car.shapeColor = "white";
+
+  //Creating sprite for wall
+  wall = createSprite(1200, 200, 60, height/2);
+  wall.shapeColor = color(80, 80, 80);
+
+  //Adding a mark for collidation of car
+  //car.debug = true;
+
+
+  console.log("Ok be calm bro, you just need to perform an algorithm");
+  print("You can do this");
+}
+
+function draw() 
+{
+  background(0); 
+
+  //Adding an algorithm for the collision detection between the car with respect to speed and wall
+  if(wall.x - car.x < (car.width + wall.width)/2)
+  {
+    car.velocityX = 0;
+    var deformation = 0.5 * weight * speed * speed/22509;
+
+    if(deformation > 180)
+    {
+      car.shapeColor = color(255, 0, 0);
+    }
+
+    if(deformation < 180 && deformation > 100)
+    {
+      car.shapeColor = color(230, 230, 0);
+    }
+
+    if(deformation < 100)
+    {
+      car.shapeColor = color(0, 255, 0);
+    }
+  }
+
+  drawSprites();
+}
